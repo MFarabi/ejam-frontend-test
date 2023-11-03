5b355b983e57e2b73553b97f9a1447e9c4724213
mansourfarabi <mansour.farabi@gmail.com>
2023-11-03 22:40:43 +0330

    feat: add icon collection

---
+++ utils/layout/theme-icon.ts
@@ -0,0 +1,7 @@
+export const icons = {
+  'checkmark-starburst-16-regular': 'fluent:checkmark-starburst-16-regular',
+  'truck-light': 'ph:truck-light',
+  'heart-outline': 'mdi:cards-heart-outline',
+  'arrow-sync-checkmark-20-regular': 'fluent:arrow-sync-checkmark-20-regular',
+  'lock-outline': 'solar:lock-outline',
+};
